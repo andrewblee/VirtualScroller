9ba1d8bb73292bbb6de60f873d0e14f703668611
Andrew Lee <prgmr27@gmail.com>
2016-01-01 21:16:30 -0800

    Moved scrolled() to virtualScroller

--- APM/virtualScroller.js
+++ APM/virtualScroller.js
@@ -176,6 +176,30 @@ function populatePartData(partIds, first, last, partsByPartId) {
 }
 
 /* HELPERS */
+(function ($) {
+    var uniqueCntr = 0;
+    $.fn.scrolled = function (waitTime, fn) {
+        if (typeof waitTime === "function") {
+            fn = waitTime;
+            waitTime = 500;
+        }
+        var tag = "scrollTimer" + uniqueCntr++;
+        this.scroll(function () {
+            var self = $(this);
+            var timer = self.data(tag);
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function () {
+                self.removeData(tag);
+                fn.call(self[0]);
+                console.log("scroll from scrolled");
+            }, waitTime);
+            self.data(tag, timer);
+        });
+    }
+})(jQuery);
+
 function getScrollBarWidth() {
     var inner = document.createElement('p');
     inner.style.width = "100%";

2406c523ca7c741c4c0b2a1b1e1fa1d202ede568
Andrew Lee <prgmr27@gmail.com>
2016-01-15 22:35:04 -0800

    Parameterize setCanvasHeight() function

--- APM/app/directiveTest/virtualScrollColumn.js
+++ APM/app/directiveTest/virtualScrollColumn.js
@@ -6,27 +6,31 @@ var app;
             this.templateUrl = 'app/directiveTest/virtual-scroll-column.html';
             this.scope = {
                 data: '=',
-                cellHeight: '='
+                cellHeight: '=',
+                cellBuffer: '=',
+                delayInMilliseconds: '='
             };
             this.restrict = 'E';
             VirtualScrollColumn.prototype.link = function ($scope, element, attributes) {
+                var DEFAULT_CELL_BUFFER = 3;
+                var DEFAULT_DELAY_IN_MILLISECONDS = 80;
+                $scope.cellBuffer = $scope.cellBuffer || DEFAULT_CELL_BUFFER;
+                $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
                 validateScope();
                 var keys = Object.keys($scope.data);
-                var CELL_BUFFER_COUNT = 3;
-                var SCROLL_RESOLUTION_MILLISECONDS = 80;
                 var $headerCol = element.find('.virtual-scroll-col');
                 var $canvas = element.find('.canvas');
-                setCanvasHeight();
+                setCanvasHeight($canvas, keys.length, $scope.cellHeight);
                 populateStandardCol();
-                element.find('.virtual-scroll-col').scrolled(SCROLL_RESOLUTION_MILLISECONDS, function () {
+                element.find('.virtual-scroll-col').scrolled($scope.delayInMilliSeconds, function () {
                     populateStandardCol();
                 });
                 function populateStandardCol() {
-                    var scrollAmount = $('.virtual-scroll-col').scrollTop();
+                    var scrollAmount = $headerCol.scrollTop();
                     var firstCell = Math.floor(scrollAmount / $scope.cellHeight);
-                    var headerColHeight = $('.virtual-scroll-col').height();
+                    var headerColHeight = $headerCol.height();
                     var numCellsShowing = Math.round(headerColHeight / $scope.cellHeight);
-                    var numCellsShowingPlusBuffer = numCellsShowing + CELL_BUFFER_COUNT;
+                    var numCellsShowingPlusBuffer = numCellsShowing + $scope.cellBuffer;
                     populateStandardData(keys, firstCell, Math.min(firstCell + numCellsShowingPlusBuffer + 1, keys.length), $scope.data);
                 }
                 function populateStandardData(keys, first, last, data) {
@@ -35,7 +39,7 @@ var app;
                         html += '<div class="virtual-scroll-col-box" style="top:' + i * $scope.cellHeight + 'px">' + data[keys[i]].name + '</div>';
                     }
                     $canvas.html(html);
-                    for (i = first - 1; i >= Math.max(first - CELL_BUFFER_COUNT, 0); i--) {
+                    for (i = first - 1; i >= Math.max(first - $scope.cellBuffer, 0); i--) {
                         $canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * $scope.cellHeight + 'px">' + $scope.data[keys[i]].name + '</div>');
                     }
                 }
@@ -46,16 +50,21 @@ var app;
                     if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
                         throw new Error('cellHeight is invalid.');
                     }
+                    if ($scope.cellBuffer === undefined || $scope.cellBuffer <= 0) {
+                        throw new Error('cellBuffer is invalid.');
+                    }
+                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                        throw new Error('delayInMilliSeconds is invalid.');
+                    }
                 }
                 /**
-                 * Set the overall canvas height.  Dependent on data length and cell height.
+                 * Set the overall canvas height.
+                 * @param canvas JQuery reference to the canvas.
+                 * @param dataLength Total number of data elements.
+                 * @param cellHeight Height in pixels of a single column cell.
                  */
-                function setCanvasHeight() {
-                    if (keys.length) {
-                        $scope.style = {
-                            'height': keys.length * $scope.cellHeight + 'px'
-                        };
-                    }
+                function setCanvasHeight(canvas, dataLength, cellHeight) {
+                    canvas.height(dataLength * cellHeight);
                 }
             };
         }
--- APM/app/directiveTest/virtualScrollColumn.ts
+++ APM/app/directiveTest/virtualScrollColumn.ts
@@ -11,8 +11,9 @@
 
     export interface IVirtualScrollColumnScope extends ng.IScope {
         data: IDictionary<any>;
-        style: {};
         cellHeight: number;
+        cellBuffer: number;
+        delayInMilliSeconds: number;
     }
 
     class VirtualScrollColumn {
@@ -20,34 +21,39 @@
         public templateUrl = 'app/directiveTest/virtual-scroll-column.html';
         public scope = {
             data: '=',
-            cellHeight: '='
+            cellHeight: '=',
+            cellBuffer: '=',
+            delayInMilliseconds: '='
         };
         public restrict = 'E';
 
         constructor() {
             VirtualScrollColumn.prototype.link = ($scope: IVirtualScrollColumnScope, element: JQuery, attributes) => {
+                let DEFAULT_CELL_BUFFER = 3;
+                let DEFAULT_DELAY_IN_MILLISECONDS = 80;
+
+                $scope.cellBuffer = $scope.cellBuffer || DEFAULT_CELL_BUFFER;
+                $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
+
                 validateScope();
 
                 let keys = Object.keys($scope.data);
-
-                let CELL_BUFFER_COUNT = 3;
-                let SCROLL_RESOLUTION_MILLISECONDS = 80;
                 let $headerCol = element.find('.virtual-scroll-col');
                 let $canvas = element.find('.canvas');
 
-                setCanvasHeight();
+                setCanvasHeight($canvas, keys.length, $scope.cellHeight);
                 populateStandardCol();
 
-                (<any>element.find('.virtual-scroll-col')).scrolled(SCROLL_RESOLUTION_MILLISECONDS, function () {
+                (<any>element.find('.virtual-scroll-col')).scrolled($scope.delayInMilliSeconds, function () {
                     populateStandardCol();
                 });
 
                 function populateStandardCol() {
-                    var scrollAmount = $('.virtual-scroll-col').scrollTop();
+                    var scrollAmount = $headerCol.scrollTop();
                     var firstCell = Math.floor(scrollAmount / $scope.cellHeight);
-                    var headerColHeight = $('.virtual-scroll-col').height();
+                    var headerColHeight = $headerCol.height();
                     var numCellsShowing = Math.round(headerColHeight / $scope.cellHeight);
-                    var numCellsShowingPlusBuffer = numCellsShowing + CELL_BUFFER_COUNT;
+                    var numCellsShowingPlusBuffer = numCellsShowing + $scope.cellBuffer;
                     populateStandardData(keys, firstCell, Math.min(firstCell + numCellsShowingPlusBuffer + 1, keys.length), $scope.data);
                 }
 
@@ -60,7 +66,7 @@
 
                     $canvas.html(html);
 
-                    for (i = first - 1; i >= Math.max(first - CELL_BUFFER_COUNT, 0); i--) {
+                    for (i = first - 1; i >= Math.max(first - $scope.cellBuffer, 0); i--) {
                         $canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * $scope.cellHeight + 'px">' + $scope.data[keys[i]].name + '</div>');
                     }
                 }
@@ -73,17 +79,24 @@
                     if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
                         throw new Error('cellHeight is invalid.');
                     }
+
+                    if ($scope.cellBuffer === undefined || $scope.cellBuffer <= 0) {
+                        throw new Error('cellBuffer is invalid.');
+                    }
+
+                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                        throw new Error('delayInMilliSeconds is invalid.');
+                    }
                 }
 
                 /**
-                 * Set the overall canvas height.  Dependent on data length and cell height.
+                 * Set the overall canvas height.
+                 * @param canvas JQuery reference to the canvas.
+                 * @param dataLength Total number of data elements.
+                 * @param cellHeight Height in pixels of a single column cell.
                  */
-                function setCanvasHeight() {
-                    if (keys.length) {
-                        $scope.style = {
-                            'height': keys.length * $scope.cellHeight + 'px'
-                        }
-                    }
+                function setCanvasHeight(canvas: JQuery, dataLength: number, cellHeight: number) {
+                    canvas.height(dataLength * cellHeight);
                 }
             };
         }

11dfd370bcb50c7fd7e4847ac1fa5c9fa1ccfd66
Andrew Lee <prgmr27@gmail.com>
2016-01-15 23:32:46 -0800

    Add parameters to populateData()

--- APM/app/directiveTest/virtualScrollColumn.js
+++ APM/app/directiveTest/virtualScrollColumn.js
@@ -11,52 +11,61 @@ var app;
                 delayInMilliseconds: '='
             };
             this.restrict = 'E';
-            VirtualScrollColumn.prototype.link = function ($scope, element, attributes) {
+            VirtualScrollColumn.prototype.link = function (scope, element, attributes) {
                 var DEFAULT_BUFFER = 3;
                 var DEFAULT_DELAY_IN_MILLISECONDS = 80;
-                $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
-                $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
+                scope.buffer = scope.buffer || DEFAULT_BUFFER;
+                scope.delayInMilliSeconds = scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
                 validateScope();
-                var keys = Object.keys($scope.data);
-                var $headerCol = element.find('.virtual-scroll-col');
-                var $canvas = element.find('.canvas');
-                setCanvasHeight($canvas, keys.length, $scope.cellHeight);
-                populateStandardCol();
-                element.find('.virtual-scroll-col').scrolled($scope.delayInMilliSeconds, function () {
-                    populateStandardCol();
+                var keys = Object.keys(scope.data);
+                var column = element.find('.virtual-scroll-col');
+                var canvas = element.find('.canvas');
+                setCanvasHeight(canvas, keys.length, scope.cellHeight);
+                populateStandardCol(column.scrollTop(), scope.cellHeight);
+                column.scrolled(scope.delayInMilliSeconds, function () {
+                    populateStandardCol(column.scrollTop(), scope.cellHeight);
                 });
-                function populateStandardCol() {
-                    var scrollAmount = $headerCol.scrollTop();
-                    var firstCell = Math.floor(scrollAmount / $scope.cellHeight);
-                    var headerColHeight = $headerCol.height();
-                    var numCellsShowing = Math.round(headerColHeight / $scope.cellHeight);
-                    var numCellsShowingPlusBuffer = numCellsShowing + $scope.buffer;
-                    var lastVisible = Math.min(firstCell + numCellsShowingPlusBuffer + 1, keys.length);
-                    populateData($scope.data, keys, firstCell, lastVisible, $scope.cellHeight, $scope.buffer);
+                function populateStandardCol(scrollAmount, cellHeight) {
+                    var firstVisible = Math.floor(scrollAmount / cellHeight);
+                    var headerColHeight = column.height();
+                    var numCellsShowing = Math.round(headerColHeight / cellHeight);
+                    var numCellsShowingPlusBuffer = numCellsShowing + scope.buffer;
+                    var lastVisible = Math.min(firstVisible + numCellsShowingPlusBuffer + 1, keys.length);
+                    populateData(canvas, scope.data, keys, firstVisible, lastVisible, cellHeight, scope.buffer);
                 }
-                function populateData(data, keys, firstVisible, lastVisible, cellHeight, buffer) {
+                /**
+                 * Function populates the data into the DOM.
+                 * @param canvas JQuery reference to the canvas.
+                 * @param data Dictionary containing data.
+                 * @param keys Dictionary keys.
+                 * @param firstVisible Index of the first visible cell.
+                 * @param lastVisible Index of the last visible cell.
+                 * @param cellHeight Height in pixels of a single column cell.
+                 * @param buffer Number of cells to insert above and below the visible cells to improve user scrolling experience.
+                 */
+                function populateData(canvas, data, keys, firstVisible, lastVisible, cellHeight, buffer) {
                     var i, length, html = '';
                     for (i = firstVisible; i < lastVisible; i++) {
                         html += '<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>';
                     }
-                    $canvas.html(html);
+                    canvas.html(html);
                     // Need to prepend these after the visible cells have been added to the DOM otherwise the buffer cells will be showing.
                     // Also add cells one by one above the visible cells so that the closest cells will be visible first when the user scrolls upward.
                     for (i = firstVisible - 1; i >= Math.max(firstVisible - buffer, 0); i--) {
-                        $canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>');
+                        canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>');
                     }
                 }
                 function validateScope() {
-                    if (!$scope.data) {
+                    if (!scope.data) {
                         throw new Error('data must be defined');
                     }
-                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
+                    if (scope.cellHeight === undefined || scope.cellHeight <= 0) {
                         throw new Error('cellHeight is invalid.');
                     }
-                    if ($scope.buffer === undefined || $scope.buffer <= 0) {
+                    if (scope.buffer === undefined || scope.buffer <= 0) {
                         throw new Error('cellBuffer is invalid.');
                     }
-                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                    if (scope.delayInMilliSeconds === undefined || scope.delayInMilliSeconds <= 0) {
                         throw new Error('delayInMilliSeconds is invalid.');
                     }
                 }
--- APM/app/directiveTest/virtualScrollColumn.ts
+++ APM/app/directiveTest/virtualScrollColumn.ts
@@ -17,7 +17,7 @@
     }
 
     class VirtualScrollColumn {
-        public link: ($scope: IVirtualScrollColumnScope, element: JQuery, attributes) => void;
+        public link: (scope: IVirtualScrollColumnScope, element: JQuery, attributes) => void;
         public templateUrl = 'app/directiveTest/virtual-scroll-column.html';
         public scope = {
             data: '=',
@@ -28,66 +28,75 @@
         public restrict = 'E';
 
         constructor() {
-            VirtualScrollColumn.prototype.link = ($scope: IVirtualScrollColumnScope, element: JQuery, attributes) => {
+            VirtualScrollColumn.prototype.link = (scope: IVirtualScrollColumnScope, element: JQuery, attributes) => {
                 let DEFAULT_BUFFER = 3;
                 let DEFAULT_DELAY_IN_MILLISECONDS = 80;
 
-                $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
-                $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
+                scope.buffer = scope.buffer || DEFAULT_BUFFER;
+                scope.delayInMilliSeconds = scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
 
                 validateScope();
 
-                let keys = Object.keys($scope.data);
-                let $headerCol = element.find('.virtual-scroll-col');
-                let $canvas = element.find('.canvas');
+                let keys = Object.keys(scope.data);
+                let column = element.find('.virtual-scroll-col');
+                let canvas = element.find('.canvas');
 
-                setCanvasHeight($canvas, keys.length, $scope.cellHeight);
-                populateStandardCol();
+                setCanvasHeight(canvas, keys.length, scope.cellHeight);
+                populateStandardCol(column.scrollTop(), scope.cellHeight);
 
-                (<any>element.find('.virtual-scroll-col')).scrolled($scope.delayInMilliSeconds, function () {
-                    populateStandardCol();
+                (<any>column).scrolled(scope.delayInMilliSeconds, function () {
+                    populateStandardCol(column.scrollTop(), scope.cellHeight);
                 });
 
-                function populateStandardCol() {
-                    var scrollAmount = $headerCol.scrollTop();
-                    var firstCell = Math.floor(scrollAmount / $scope.cellHeight);
-                    var headerColHeight = $headerCol.height();
-                    var numCellsShowing = Math.round(headerColHeight / $scope.cellHeight);
-                    var numCellsShowingPlusBuffer = numCellsShowing + $scope.buffer;
-                    var lastVisible = Math.min(firstCell + numCellsShowingPlusBuffer + 1, keys.length);
-                    populateData($scope.data, keys, firstCell, lastVisible, $scope.cellHeight, $scope.buffer);
+                function populateStandardCol(scrollAmount: number, cellHeight: number) {
+                    let firstVisible = Math.floor(scrollAmount / cellHeight);
+                    let headerColHeight = column.height();
+                    let numCellsShowing = Math.round(headerColHeight / cellHeight);
+                    let numCellsShowingPlusBuffer = numCellsShowing + scope.buffer;
+                    let lastVisible = Math.min(firstVisible + numCellsShowingPlusBuffer + 1, keys.length);
+                    populateData(canvas, scope.data, keys, firstVisible, lastVisible, cellHeight, scope.buffer);
                 }
 
-                function populateData(data: IDictionary<any>, keys: string[], firstVisible: number, lastVisible: number, cellHeight: number, buffer: number) {
+                /**
+                 * Function populates the data into the DOM.
+                 * @param canvas JQuery reference to the canvas.
+                 * @param data Dictionary containing data.
+                 * @param keys Dictionary keys.
+                 * @param firstVisible Index of the first visible cell.
+                 * @param lastVisible Index of the last visible cell.
+                 * @param cellHeight Height in pixels of a single column cell.
+                 * @param buffer Number of cells to insert above and below the visible cells to improve user scrolling experience.
+                 */
+                function populateData(canvas: JQuery, data: IDictionary<any>, keys: string[], firstVisible: number, lastVisible: number, cellHeight: number, buffer: number) {
                     let i, length, html = '';
 
                     for (i = firstVisible; i < lastVisible; i++) {
                         html += '<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>';
                     }
 
-                    $canvas.html(html);
+                    canvas.html(html);
 
                     // Need to prepend these after the visible cells have been added to the DOM otherwise the buffer cells will be showing.
                     // Also add cells one by one above the visible cells so that the closest cells will be visible first when the user scrolls upward.
                     for (i = firstVisible - 1; i >= Math.max(firstVisible - buffer, 0); i--) {
-                        $canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>');
+                        canvas.prepend('<div class="virtual-scroll-col-box" style="top:' + i * cellHeight + 'px">' + data[keys[i]].name + '</div>');
                     }
                 }
 
                 function validateScope() {
-                    if (!$scope.data) {
+                    if (!scope.data) {
                         throw new Error('data must be defined');
                     }
 
-                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
+                    if (scope.cellHeight === undefined || scope.cellHeight <= 0) {
                         throw new Error('cellHeight is invalid.');
                     }
 
-                    if ($scope.buffer === undefined || $scope.buffer <= 0) {
+                    if (scope.buffer === undefined || scope.buffer <= 0) {
                         throw new Error('cellBuffer is invalid.');
                     }
 
-                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                    if (scope.delayInMilliSeconds === undefined || scope.delayInMilliSeconds <= 0) {
                         throw new Error('delayInMilliSeconds is invalid.');
                     }
                 }

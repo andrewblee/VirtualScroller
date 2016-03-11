b500fb67dc64a0e0d2f6a448e82d50a23d48c436
Andrew Lee <prgmr27@gmail.com>
2016-03-10 23:05:41 -0800

    Grid template in place.

--- APM/app/virtualScroller/virtualScroller.js
+++ APM/app/virtualScroller/virtualScroller.js
@@ -6,13 +6,77 @@ var app;
             this.templateUrl = 'app/virtualScroller/virtualScroller.html';
             this.scope = {
                 header: '=',
-                arr: '='
+                arr: '=',
+                cellHeight: '=',
+                buffer: '=',
+                delayInMilliseconds: '=',
+                top: '=',
+                bottom: '=',
+                left: '=',
+                colWidth: '='
             };
             this.restrict = 'E';
             this.transclude = true;
             VirtualScroller.prototype.link = function ($scope, element, attributes) {
+                var $column = element.find('.virtual-scroll-col');
+                var $canvas = element.find('.canvas');
                 $scope.header = 'hi from directive';
                 $scope.arr = [1, 2, 3, 4];
+                setDefaultValues();
+                validateScope();
+                configureStyle();
+                function configureStyle() {
+                    $scope.colStyle = {
+                        'position': 'absolute',
+                        'top': $scope.top,
+                        'bottom': $scope.bottom,
+                        'left': $scope.left,
+                        'width': $scope.colWidth,
+                        'overflow-x': 'hidden',
+                        'overflow-y': 'auto'
+                    };
+                    $scope.canvasStyle = {
+                        'position': 'relative'
+                    };
+                }
+                function setDefaultValues() {
+                    var DEFAULT_BUFFER = 3;
+                    var DEFAULT_DELAY_IN_MILLISECONDS = 80;
+                    var DEFAULT_BOT = 0;
+                    var DEFAULT_LEFT = 0;
+                    var DEFAULT_TOP = 0;
+                    $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
+                    $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
+                    $scope.bottom = $scope.bottom || DEFAULT_BOT;
+                    $scope.left = $scope.left || DEFAULT_LEFT;
+                    $scope.top = $scope.top || DEFAULT_TOP;
+                }
+                function validateScope() {
+                    //if (!$scope.data) {
+                    //    throw new Error('data must be defined.');
+                    //}
+                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
+                        throw new Error('cellHeight is invalid.');
+                    }
+                    if ($scope.buffer === undefined || $scope.buffer < 0) {
+                        throw new Error('cellBuffer is invalid.');
+                    }
+                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                        throw new Error('delayInMilliSeconds is invalid.');
+                    }
+                    if ($scope.top === undefined || $scope.top < 0) {
+                        throw new Error('top is invalid.');
+                    }
+                    if ($scope.left === undefined || $scope.left < 0) {
+                        throw new Error('left is invalid.');
+                    }
+                    if ($scope.bottom === undefined || $scope.bottom < 0) {
+                        throw new Error('bottom is invalid.');
+                    }
+                    if ($scope.colWidth === undefined || $scope.colWidth <= 0) {
+                        throw new Error('cellWidth is invalid.');
+                    }
+                }
             };
         }
         VirtualScroller.Factory = function () {
--- APM/app/virtualScroller/virtualScroller.ts
+++ APM/app/virtualScroller/virtualScroller.ts
@@ -5,6 +5,15 @@
         person: any;
         header: string;
         arr: Array<number>;
+        cellHeight: number;
+        buffer: number;
+        delayInMilliSeconds: number;
+        colStyle: {};
+        canvasStyle: {};
+        top: number;
+        bottom: number;
+        left: number;
+        colWidth: number;
     }
 
     class VirtualScroller {
@@ -12,15 +21,93 @@
         public templateUrl = 'app/virtualScroller/virtualScroller.html';
         public scope = {
             header: '=',
-            arr: '='
+            arr: '=',
+            cellHeight: '=',
+            buffer: '=',
+            delayInMilliseconds: '=',
+            top: '=',
+            bottom: '=',
+            left: '=',
+            colWidth: '='
         };
         public restrict = 'E';
         public transclude = true;
 
         constructor() {
             VirtualScroller.prototype.link = ($scope: IVirtualScrollerScope, element: JQuery, attributes) => {
+                let $column = element.find('.virtual-scroll-col');
+                let $canvas = element.find('.canvas');
+
                 $scope.header = 'hi from directive';
                 $scope.arr = [1, 2, 3, 4];
+
+                setDefaultValues();
+                validateScope();
+                configureStyle();
+
+                function configureStyle(): void {
+                    $scope.colStyle = {
+                        'position': 'absolute',
+                        'top': $scope.top,
+                        'bottom': $scope.bottom,
+                        'left': $scope.left,
+                        'width': $scope.colWidth,
+                        'overflow-x': 'hidden',
+                        'overflow-y': 'auto'
+                    };
+
+                    $scope.canvasStyle = {
+                        'position': 'relative'
+                    }
+                }
+
+                function setDefaultValues(): void {
+                    let DEFAULT_BUFFER = 3;
+                    let DEFAULT_DELAY_IN_MILLISECONDS = 80;
+                    let DEFAULT_BOT = 0;
+                    let DEFAULT_LEFT = 0;
+                    let DEFAULT_TOP = 0;
+
+                    $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
+                    $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
+                    $scope.bottom = $scope.bottom || DEFAULT_BOT;
+                    $scope.left = $scope.left || DEFAULT_LEFT;
+                    $scope.top = $scope.top || DEFAULT_TOP;
+                }
+
+                function validateScope(): void {
+                    //if (!$scope.data) {
+                    //    throw new Error('data must be defined.');
+                    //}
+
+                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
+                        throw new Error('cellHeight is invalid.');
+                    }
+
+                    if ($scope.buffer === undefined || $scope.buffer < 0) {
+                        throw new Error('cellBuffer is invalid.');
+                    }
+
+                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
+                        throw new Error('delayInMilliSeconds is invalid.');
+                    }
+
+                    if ($scope.top === undefined || $scope.top < 0) {
+                        throw new Error('top is invalid.');
+                    }
+
+                    if ($scope.left === undefined || $scope.left < 0) {
+                        throw new Error('left is invalid.');
+                    }
+
+                    if ($scope.bottom === undefined || $scope.bottom < 0) {
+                        throw new Error('bottom is invalid.');
+                    }
+
+                    if ($scope.colWidth === undefined || $scope.colWidth <= 0) {
+                        throw new Error('cellWidth is invalid.');
+                    }
+                }
             };
         }
 

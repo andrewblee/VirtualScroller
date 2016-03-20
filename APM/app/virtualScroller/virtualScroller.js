d1d09d908566be94137b5e6a4a25f7a0254ac79e
Andrew Lee <prgmr27@gmail.com>
2016-03-19 23:36:15 -0700

    Cleaned up virtualScroller.ts.

--- APM/app/virtualScroller/virtualScroller.js
+++ APM/app/virtualScroller/virtualScroller.js
@@ -20,10 +20,6 @@ var app;
             this.replace = true;
             VirtualScroller.prototype.link = function ($scope, element, attributes) {
                 var canvas = element.children();
-                setDefaultValues();
-                validateScope();
-                configureStyle();
-                //setCanvasHeight();
                 setVirtualData();
                 element.scrolled($scope.delayInMilliSeconds, function () {
                     setVirtualData();
@@ -33,23 +29,9 @@ var app;
                     setVirtualData();
                     $scope.$apply();
                 });
-                function configureStyle() {
-                    //$scope.colStyle = {
-                    //    'position': 'absolute',
-                    //    'top': $scope.top,
-                    //    'bottom': $scope.bottom,
-                    //    'left': $scope.left,
-                    //    'width': $scope.colWidth,
-                    //    'overflow-x': 'hidden',
-                    //    'overflow-y': 'auto'
-                    //};
-                    //$scope.canvasStyle = {
-                    //    'position': 'relative',
-                    //}
-                }
                 function setVirtualData() {
-                    if (!canvas.height()) {
-                        canvas.height($scope.arr.length * $scope.cellHeight);
+                    if (!$scope.canvasStyle) {
+                        setCanvasHeight();
                     }
                     var firstVisible = Math.floor(element.scrollTop() / $scope.cellHeight);
                     var visibleColHeight = element.height();
@@ -63,45 +45,9 @@ var app;
                     }
                 }
                 function setCanvasHeight() {
-                    canvas.height($scope.arr.length * $scope.cellHeight);
-                }
-                function setDefaultValues() {
-                    var DEFAULT_BUFFER = 3;
-                    var DEFAULT_DELAY_IN_MILLISECONDS = 80;
-                    var DEFAULT_BOT = 0;
-                    var DEFAULT_LEFT = 0;
-                    var DEFAULT_TOP = 0;
-                    $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
-                    $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
-                    $scope.bottom = $scope.bottom || DEFAULT_BOT;
-                    $scope.left = $scope.left || DEFAULT_LEFT;
-                    $scope.top = $scope.top || DEFAULT_TOP;
-                }
-                function validateScope() {
-                    //if (!$scope.data) {
-                    //    throw new Error('data must be defined.');
-                    //}
-                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
-                        throw new Error('cellHeight is invalid.');
-                    }
-                    if ($scope.buffer === undefined || $scope.buffer < 0) {
-                        throw new Error('cellBuffer is invalid.');
-                    }
-                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
-                        throw new Error('delayInMilliSeconds is invalid.');
-                    }
-                    if ($scope.top === undefined || $scope.top < 0) {
-                        throw new Error('top is invalid.');
-                    }
-                    if ($scope.left === undefined || $scope.left < 0) {
-                        throw new Error('left is invalid.');
-                    }
-                    if ($scope.bottom === undefined || $scope.bottom < 0) {
-                        throw new Error('bottom is invalid.');
-                    }
-                    if ($scope.colWidth === undefined || $scope.colWidth <= 0) {
-                        throw new Error('cellWidth is invalid.');
-                    }
+                    $scope.canvasStyle = {
+                        height: $scope.arr.length * $scope.cellHeight
+                    };
                 }
             };
         }
--- APM/app/virtualScroller/virtualScroller.ts
+++ APM/app/virtualScroller/virtualScroller.ts
@@ -39,10 +39,6 @@
             VirtualScroller.prototype.link = ($scope: IVirtualScrollerScope, element: JQuery, attributes) => {
                 let canvas = element.children();
 
-                setDefaultValues();
-                validateScope();
-                configureStyle();
-                //setCanvasHeight();
                 setVirtualData();
 
                 (<any>element).scrolled($scope.delayInMilliSeconds, function () {
@@ -55,25 +51,9 @@
                     $scope.$apply();
                 });
 
-                function configureStyle(): void {
-                    //$scope.colStyle = {
-                    //    'position': 'absolute',
-                    //    'top': $scope.top,
-                    //    'bottom': $scope.bottom,
-                    //    'left': $scope.left,
-                    //    'width': $scope.colWidth,
-                    //    'overflow-x': 'hidden',
-                    //    'overflow-y': 'auto'
-                    //};
-
-                    //$scope.canvasStyle = {
-                    //    'position': 'relative',
-                    //}
-                }
-
                 function setVirtualData() {
-                    if (!canvas.height()) {
-                        canvas.height($scope.arr.length * $scope.cellHeight);
+                    if (!$scope.canvasStyle) {
+                        setCanvasHeight();
                     }
 
                     let firstVisible = Math.floor(element.scrollTop() / $scope.cellHeight);
@@ -91,54 +71,8 @@
                 }
 
                 function setCanvasHeight(): void {
-                    canvas.height($scope.arr.length * $scope.cellHeight);
-                }
-
-                function setDefaultValues(): void {
-                    let DEFAULT_BUFFER = 3;
-                    let DEFAULT_DELAY_IN_MILLISECONDS = 80;
-                    let DEFAULT_BOT = 0;
-                    let DEFAULT_LEFT = 0;
-                    let DEFAULT_TOP = 0;
-
-                    $scope.buffer = $scope.buffer || DEFAULT_BUFFER;
-                    $scope.delayInMilliSeconds = $scope.delayInMilliSeconds || DEFAULT_DELAY_IN_MILLISECONDS;
-                    $scope.bottom = $scope.bottom || DEFAULT_BOT;
-                    $scope.left = $scope.left || DEFAULT_LEFT;
-                    $scope.top = $scope.top || DEFAULT_TOP;
-                }
-
-                function validateScope(): void {
-                    //if (!$scope.data) {
-                    //    throw new Error('data must be defined.');
-                    //}
-
-                    if ($scope.cellHeight === undefined || $scope.cellHeight <= 0) {
-                        throw new Error('cellHeight is invalid.');
-                    }
-
-                    if ($scope.buffer === undefined || $scope.buffer < 0) {
-                        throw new Error('cellBuffer is invalid.');
-                    }
-
-                    if ($scope.delayInMilliSeconds === undefined || $scope.delayInMilliSeconds <= 0) {
-                        throw new Error('delayInMilliSeconds is invalid.');
-                    }
-
-                    if ($scope.top === undefined || $scope.top < 0) {
-                        throw new Error('top is invalid.');
-                    }
-
-                    if ($scope.left === undefined || $scope.left < 0) {
-                        throw new Error('left is invalid.');
-                    }
-
-                    if ($scope.bottom === undefined || $scope.bottom < 0) {
-                        throw new Error('bottom is invalid.');
-                    }
-
-                    if ($scope.colWidth === undefined || $scope.colWidth <= 0) {
-                        throw new Error('cellWidth is invalid.');
+                    $scope.canvasStyle = {
+                        height: $scope.arr.length * $scope.cellHeight
                     }
                 }
             };
